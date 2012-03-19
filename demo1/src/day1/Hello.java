571a494d469a06d9a2cb0811c553d28b9d83b85f
zhonglihua <lihua.zhong@dragonflow.com>
2012-03-19 11:56:44 +0800

    first commit

---
+++ demo1/src/day1/Hello.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hjk;ls");
+	}
+
+}
